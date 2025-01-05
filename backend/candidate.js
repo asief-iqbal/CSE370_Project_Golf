0e336f7d94e137d0443807d2047dd24bdd27acb4
Asief Iqbal Dieyaz <147961056+asief-iqbal@users.noreply.github.com>
2025-01-06 00:15:42 +0600

    Delete backend directory

--- backend/candidate.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const EmployeeSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    userType: String
-})
-const EmployeeModel = mongoose.model("Employee", EmployeeSchema);
-export default EmployeeModel;
